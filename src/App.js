87668dc635c381fc34b8b125c241ec5176161f46
sadvika7reddy <sadvika7reddy>
2023-01-09 14:06:25 +0530

    Add button

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react'
+import Header from './components/LayOut/Header';
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/LayOut/Header.js
@@ -0,0 +1,17 @@
+import React,{Fragment} from 'react';
+import classes from './Header.module.css';
+import CartButton from './CartButton';
+const Header=()=>{
+    return(
+    <Fragment>   
+        <header className={classes.header}>
+            <h1>React Order</h1>
+            <CartButton />
+        </header>
+        <div className={classes.image}>
+            <img src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg"/>
+        </div>
+    </Fragment>    
+    )
+}
+export default Header;
